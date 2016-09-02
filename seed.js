937e862020284cbd4105f9b0c611d831cfb32ea1
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-01 19:02:07 -0700

    updates endpaths

--- seed.js
+++ seed.js
@@ -2,22 +2,37 @@
 // simply run: `node seed.js` from the root of this project folder.
 
 var db = require('./models');
-// 
-// var Profile = require('./models/profile');
-//
-// var mishaLeClair = new Profile({
-//   name: "Misha LeClair",
-//   githubLink: "https://github.com/sfnewzgirl",
-//   githubProfileImage: "https://avatars1.githubusercontent.com/u/19892743?v=3&s=460",
-//   personalSiteLink: "https://sfnewzgirl.github.io/",
-//   currentCity: "Concord",
-//   pets: "1 fish"
-// });
-//
-// mishaLeClair.save(function(err, Profile){
-//   if(err) {return console.log(err);}
-//   console.log("saved profile: ", Profile);
-// });
+
+var Profile = require('./models/profile');
+
+var mishaLeClair = new Profile({
+  name: "Misha LeClair",
+  githubLink: "https://github.com/sfnewzgirl",
+  githubProfileImage: "https://avatars1.githubusercontent.com/u/19892743?v=3&s=460",
+  personalSiteLink: "https://sfnewzgirl.github.io/",
+  currentCity: "Concord",
+  pets: "1 fish"
+});
+
+mishaLeClair.save(function(err, Profile){
+  if(err) {return console.log(err);}
+  console.log("saved profile: ", Profile);
+});
+
+var Career = require('.models/careers');
+
+var career_list = [
+  {
+    title: String,
+    company: String,
+    accomplishment: String
+  },
+  {
+    title: String,
+    company: String,
+    accomplishment: String
+},
+];
 // var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
 // db.Campsite.create(new_campsite, function(err, campsite){
--- server.js
+++ server.js
@@ -21,22 +21,6 @@ app.use(function(req, res, next) {
 
 var db = require('./models');
 
-// var Profile = require('./models/profile');
-//
-// var mishaLeClair = new Profile({
-//   name: "Misha LeClair",
-//   githubLink: "https://github.com/sfnewzgirl",
-//   githubProfileImage: "https://avatars1.githubusercontent.com/u/19892743?v=3&s=460",
-//   personalSiteLink: "https://sfnewzgirl.github.io/",
-//   currentCity: "Concord",
-//   pets: "1 fish"
-// });
-
-// mishaLeClair.save(function(err, Profile){
-//   if(err) {return console.log(err);}
-//   console.log("saved profile: ", Profile);
-// });
-
 /**********
  * ROUTES *
  **********/
@@ -53,6 +37,19 @@ app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/profile', function (req, res) {
+  db.Profile.find(function(err, books){
+    if (err) { return console.log("index error: " + err); }
+    res.json(books);
+  });
+});
+
+app.get('/api/careers', function (req, res) {
+  db.Careers.find(function(err, books){
+    if (err) { return console.log("index error: " + err); }
+    res.json(books);
+  });
+});
 /*
  * JSON API Endpoints
  */
