3db4704025a284937889e8e24ee235b396f0a24d
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-01 17:21:41 -0700

    creates profile, careers schema, adds profile into in server.js

---
+++ models/profile.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var ProfileSchema = new Schema({
+    name: String,
+    githubLink: String,
+    githubProfileImage: String,
+    personalSiteLink: String,
+    currentCity: String,
+    pets: String
+});
+
+var Profile = mongoose.model('Profile', ProfileSchema);
+module.exports = Profile;
