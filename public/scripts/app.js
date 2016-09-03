dc33b472b9994b21e9951e3958a6632a4adb7310
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-02 19:03:59 -0700

    ajax call testing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,47 @@
 console.log("Sanity Check: JS is working!");
+var template
 
 $(document).ready(function(){
-
-var source = $('#profile-template').html();
-
-var template = Handlebars.compile(source);
-
-var profileHtml = template({ profile: .json });
-
-console.log(profileHtml);
-
-$("#profile").append(profileHtml);
-
+  var source = $('#profile-template').html();
+  template = Handlebars.compile(source);
+  $.ajax({
+        method:'GET',
+        url: '/api/profile',
+        dataType: 'json',
+        success: onSuccess,
+        error: handleError
+      });
 });
+//
+// function render () {
+//   $("#profileItem").empty();
+//   var profileHtml = template({
+//     name: json.name,
+//     githubLink: json.githubLink,
+//     githubProfileImage: json.githubProfileImage,
+//     personalSiteLink: json.personalSiteLink,
+//     currentCity: json.currentCity,
+//     pets: json.pets
+//   })
+//    $("#profile").append(profileHtml);
+// };
+
+function onSuccess(json) {
+  $("#profileItem").empty();
+  var profileHtml = template({    });
+  // var profileHtml = template({
+  //      name: json,
+  //      githubLink: json,
+  //      githubProfileImage: json,
+  //      personalSiteLink: json,
+  //      currentCity: json,
+  //      pets: json
+  //    })
+  $("#profile").append(profileHtml);
+  // render ();
+  console.log(json);
+}
+
+function handleError(e) {
+    console.log(e);
+}
