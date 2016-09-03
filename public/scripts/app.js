4c5e7e7f78780744a5b2724b19d24eae7fa17025
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-02 17:47:53 -0700

    preps handlebars in app.js, html for profile

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,14 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+var source = $('#profile-template').html();
+
+var template = Handlebars.compile(source);
+
+var profileHtml = template({ profile: .json });
+
+console.log(profileHtml);
+
+$("#profile").append(profileHtml);
 
 });
