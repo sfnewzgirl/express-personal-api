16c921fd6f9f1385a6f6061d827abad640c9c161
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-04 17:58:19 -0700

    working edit form and submit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -44,6 +44,10 @@ $(document).ready(function(){
   careerList.on('click', '.editBtn', function() {
     event.preventDefault();
     $('.form-edit-career').show();
+  });
+
+  careerList.on('submit', '.saveBtn', function(event) {
+    event.preventDefault();
     $.ajax({
       method: 'PUT',
       url: '/api/careers/'+$(this).attr('data-id'),
@@ -95,37 +99,23 @@ function newCareerSuccess(json) {
 }
 
 function editCareerSuccess(json) {
-  var editedTitle = $('.form-edit-career').title;
-  var editedCompany = $('.form-edit-career').company;
-  var editedAccomplishment = $('.form-edit-career').accomplishment;
-  var editCareerHtml = careerProfile({
-    title: editedTitle,
-    company: editedCompany,
-    accomplishment: editedAccomplishment
-  })
-  $("#career-list").append(editCareerHtml);
-  $('#newCareer').on('submit', function(event) {
-    event.preventDefault();
-//     $('.form-edit-career').hide();
-  // allCareers.push(json);
-  // render();
-  // var career = json;
-  // var careerId = career._id;
-  // for (var i = 0; i < allCareers.length; i++) {
-  //   if (allCareers[i]._id === careerId) {
-  //     allCareers[i] = career;
-  //     break;
-  //   }
-  // }
-  // render();
-  });
-}
+  $('.form-edit-career').hide();
+  var career = json;
+  var careerId = career._id;
+  for (var i = 0; i < allCareers.length; i++) {
+    if (allCareers[i]._id === careerId) {
+       allCareers[i] = career;
+       break;
+     }
+   }
+  render();
+};
 
 function deleteCareerSuccess(json) {
   var career = json;
   var careerId = career._id;
-  for(var index = 0; index < allCareers.length; index++) {
-    if(allCareers[index]._id === careerId) {
+  for (var index = 0; index < allCareers.length; index++) {
+    if (allCareers[index]._id === careerId) {
       allCareers.splice(index, 1);
       break;
     }
